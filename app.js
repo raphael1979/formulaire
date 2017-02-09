c98b66dbb7b8e0705da5e8d3ff5572eb9ba5e397
raphael <moudens.raphael@orange.fr>
2017-02-09 15:47:37 +0100

    modif emailet app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var email = document.getElementById("email");
-
-email.addEventListener("keyup", function (event) {
-  if(email.validity.typeMismatch) {
-    email.setCustomValidity("Nous voudrions une adresse e-mail.");
-  } else {
-    email.setCustomValidity("");
-  }
-});
